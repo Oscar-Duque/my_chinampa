3d131ad92e5ce63ecbea2339f8785795a4f4494d
Agathe Laburthe TOlra <agathe.laburthetolra@gmail.com>
2020-11-26 16:24:04 -0600

    added the toggle button

--- app/javascript/packs/toggle_btn_submit.js
+++ app/javascript/packs/toggle_btn_submit.js
@@ -1,6 +1,7 @@
 
 const toggle = document.querySelector("#customSwitch1");
 
-toggle.addEventListener('click', () => {
+toggle.addEventListener('click', (event) => {
+  event.preventDefault();
   document.querySelector(".simple_form.edit_reminder").submit();
 })
