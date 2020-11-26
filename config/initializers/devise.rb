a46d2ce64d49a0de4d8eb9c89ec021b6d0441cf3
Corentin Julienne <corentin.julienne92@gmail.com>
2020-11-26 14:52:29 -0600

    WIP forms homepage

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,6 +9,9 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+
+  config.http_authenticatable_on_xhr = false
+  config.navigational_formats = [:"*/*", "*/*", :html, :js]
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
