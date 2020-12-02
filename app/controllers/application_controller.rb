0f100599d6404eb9f4bf10cdb15d5920a01d4914
Oscar Duque <dukescar@gmail.com>
2020-12-02 15:38:59 -0600

    fixed the navbar avatar and the home sign up form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
   include Pundit
 
   # Pundit: white-list approach.
@@ -12,6 +13,22 @@ class ApplicationController < ActionController::Base
   #   flash[:alert] = "You are not authorized to perform this action."
   #   redirect_to(root_path)
   # end
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :first_name, :photo])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    # devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
+
+  #def after_sign_in_path_for(resource)
+    #user_plants_path
+  #end
+
+  def after_sign_up_path_for(resource)
+    user_plants_path
+  end
 
 
   private
