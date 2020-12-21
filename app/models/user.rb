a8a06fb02d02ba97eafa85adaaac8122b832b5b2
Agathe Laburthe TOlra <agathe.laburthetolra@gmail.com>
2020-12-20 22:06:41 -0600

    tried some for the form. not working

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ApplicationRecord
   has_many :reminders, through: :user_plants
   has_many :plants, through: :user_plants
 
+  # validates_length_of :password, :minimum => 6, :message => "You need to have at least a 6 characters password"
   validates :photo, presence: true
 
   has_one_attached :photo
-
 end
